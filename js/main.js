53263a8ec5abc57d738a8930380b2f495a0ed256
braynaci24 <aynacimuhammetemin@gmail.com>
2021-05-15 18:58:08 +0300

    alert

--- js/main.js
+++ js/main.js
@@ -38,9 +38,7 @@ $(document).ready(function () {
                 showCloseButton: true,
                 showCancelButton: true,
                 focusConfirm: false,
-                confirmButtonText:  `<i class="fa fa-thumbs-up"></i> Great!`,
                 confirmButtonAriaLabel: 'Thumbs up, great!',
-                cancelButtonText: '<i class="fa fa-thumbs-down"></i>',
                 cancelButtonAriaLabel: 'Thumbs down'
             })
 
